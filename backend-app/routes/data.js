d64a24427f3a24a5f31272c3d506907d7e5a6622
hamca886 <ham_car@hotmail.com>
2018-03-21 14:23:59 +0100

    boilerplate

---
+++ backend-app/routes/data.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ content:[
+        {a:1},
+        {b:2}
+    ] }));
+});
+
+module.exports = router;
